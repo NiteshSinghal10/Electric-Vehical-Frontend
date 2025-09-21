76ee0704c4046bc8a9a2d572e9705acc1bde7570
bhuwan chandra kandpal <bhuuwe@bhuwans-MacBook-Air.local>
2025-09-21 23:03:37 +0530

    add routes

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+///register app routes here
+class AppPages {
+  AppPages._();
+
+  static final List<GetPage<dynamic>> routes = [];
+}
---
+++ lib/core/routes/route_names.dart
@@ -0,0 +1,6 @@
+/// add route names here
+abstract class RouteNames {
+  RouteNames._();
+
+  static const splash = '/splash';
+}
